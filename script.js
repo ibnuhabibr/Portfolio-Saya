2c6ff27bcf6142195c9259c6fb52a91e1838b51e
ibnuhabibr <ibnuhabib017@gmail.com>
2024-09-03 16:04:17 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for reaching out!');
+    this.reset();
+});
